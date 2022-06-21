2f8084a61d788eb5e520d8cbd3a2fe63bc02fada
Alisson Youssf <youssfbr@gmail.com>
2022-06-20 21:50:38 -0300

    Project hr-payroll created

--- hr-worker/src/main/java/com/github/youssfbr/hrworker/repositories/IWorkerRepository.java
+++ hr-worker/src/main/java/com/github/youssfbr/hrworker/repositories/IWorkerRepository.java
@@ -1,2 +1,7 @@
-package com.github.youssfbr.hrworker.repositories;public interface IWorkerRepository {
+package com.github.youssfbr.hrworker.repositories;
+
+import com.github.youssfbr.hrworker.entities.Worker;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IWorkerRepository extends JpaRepository<Worker, Long> {
 }
--- hr-worker/src/main/java/com/github/youssfbr/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/github/youssfbr/hrworker/resources/WorkerResource.java
@@ -1,2 +1,31 @@
-package com.github.youssfbr.hrworker.resources;public class WorkerResource {
+package com.github.youssfbr.hrworker.resources;
+
+import com.github.youssfbr.hrworker.entities.Worker;
+import com.github.youssfbr.hrworker.repositories.IWorkerRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("workers")
+public class WorkerResource {
+
+    private IWorkerRepository workerRepository;
+
+    public WorkerResource(IWorkerRepository workerRepository) {
+        this.workerRepository = workerRepository;
+    }
+
+    @GetMapping
+    public List<Worker> findAll() {
+        return workerRepository.findAll();
+    }
+
+    @GetMapping("{workerId}")
+    public Worker findById(@PathVariable Long workerId) {
+        return workerRepository.findById(workerId).orElse(null);
+    }
 }
